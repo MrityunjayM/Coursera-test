23022cbf65e8a8325be921cf921758a9b5867c58
MrityunjayM <mrityunjaymishra@hotmail.com>
2020-08-20 18:36:14 +0530

    script updated

--- Magic-view/Js_script/script.js
+++ Magic-view/Js_script/script.js
@@ -2,12 +2,18 @@ alert("This website is only for practice purpose.")
 // Main content....
 function prnt(){
     alert("This will damage your device.")
+    var i= 0;
     var ctnt=document.getElementById("inp").value;
     console.log(ctnt)
     document.getElementById("prnt").textContent=ctnt;
     function msg(){    
-    while (true) {
+    while (i<250,i++) {
+        if (ctnt=== undefined){
+            return document.getElementById("prnt").textContent="Enter Somthing.."
+        }
+        else{
         document.getElementById("prnt").textContent=ctnt;
+        }
     }};
     msg();
 }
